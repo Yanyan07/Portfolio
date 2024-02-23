cd7e0983f49d526097c3863e216a360203a0b271
Yanyan07 <yenj2015@gmail.com>
2024-02-23 11:33:55 -0700

    update tech stack

--- src/components/blackjack/BJStack.jsx
+++ src/components/blackjack/BJStack.jsx
@@ -7,16 +7,19 @@ export default function BJStack(props) {
   return (
     <div className="tech-container">
       <div>
-        Java 1.8,&nbsp;&nbsp;&nbsp;&nbsp; Spring Boot 2.7.11,&nbsp;&nbsp;&nbsp;&nbsp; 
-        RESTful API,&nbsp;&nbsp;&nbsp;&nbsp; Maven&nbsp;&nbsp;&nbsp;&nbsp;
+        Java,&nbsp;&nbsp;&nbsp;&nbsp; Spring Boot,&nbsp;&nbsp;&nbsp;&nbsp; 
+        RESTful API,&nbsp;&nbsp;&nbsp;&nbsp; Maven,&nbsp;&nbsp;&nbsp;&nbsp;
+        Tomcat&nbsp;&nbsp;&nbsp;&nbsp;
       </div>
         MySQL,&nbsp;&nbsp;&nbsp;&nbsp; JPA, &nbsp;&nbsp;&nbsp;&nbsp;
-        JavaScript,&nbsp;&nbsp;&nbsp;&nbsp; React 18&nbsp;&nbsp;&nbsp;&nbsp; 
+        Postman, &nbsp;&nbsp;&nbsp;&nbsp; Axios,&nbsp;&nbsp;&nbsp;&nbsp; 
+        JavaScript&nbsp;&nbsp;&nbsp;&nbsp;   
       <div>
       </div>
       <div>
-        axios,&nbsp;&nbsp;&nbsp;&nbsp; HTML 5,&nbsp;&nbsp;&nbsp;&nbsp; CSS,&nbsp;&nbsp;&nbsp;&nbsp; 
-        Git,&nbsp;&nbsp;&nbsp;&nbsp; GitHub &nbsp;&nbsp;&nbsp;&nbsp;
+        React,&nbsp;&nbsp;&nbsp;&nbsp; HTML5,&nbsp;&nbsp;&nbsp;&nbsp; 
+        CSS,&nbsp;&nbsp;&nbsp;&nbsp; Git,&nbsp;&nbsp;&nbsp;&nbsp; 
+        GitHub &nbsp;&nbsp;&nbsp;&nbsp;
       </div>
       <div className='close' onClick={closeTechShow}>
         <img src={require('../../images/error.png')} alt='profile'/> 
