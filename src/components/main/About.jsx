888a9c928aa3c277f44a38546605489be9eb93b4
Yanyan07 <yenj2015@gmail.com>
2025-02-02 16:34:01 -0600

    update README

--- src/components/main/About.jsx
+++ src/components/main/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
     <div className="about">
       <div>About Me</div>
       <div>
-        I'm a highly motivated software developer proficient in Java/J2EE, SQL, JPA, REST API, Spring, Spring Boot, JavaScript, React, and Angular. Having hands-on experience in java full-stack development, I've independently built Java full-stack projects. Alongside my Master's degree in biology, I hold certification as an Oracle Certified Associate, Java SE 8 Programmer. My strengths include strong collaboration skills and the ability to thrive in team environments.
+        I'm a highly motivated software developer proficient in Java/J2EE, SQL, JPA, REST API, Spring, Spring Boot, JavaScript, React, and Angular. Having hands-on experience in java full-stack development, I've independently built Java full-stack projects. Alongside my Bachelor's degree in biology, I hold certification as an Oracle Certified Associate, Java SE 8 Programmer. My strengths include strong collaboration skills and the ability to thrive in team environments.
       </div>
     </div>
   )
